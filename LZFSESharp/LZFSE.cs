72d7240ef86172ac6946e91b50e80b13ec7809ee
Crauzer <filip.quitko@gmail.com>
2017-12-30 03:18:25 +0100

    docs

--- LZFSESharp/LZFSE.cs
+++ LZFSESharp/LZFSE.cs
@@ -4,6 +4,9 @@ using System.Runtime.InteropServices;
 
 namespace LZFSESharp
 {
+    /// <summary>
+    /// Provides functions for compressing and decompressing LZFSE data
+    /// </summary>
     public static class LZFSE
     {
         [DllImport("lzfse.dll")]
@@ -12,6 +15,10 @@ namespace LZFSESharp
         [DllImport("lzfse.dll")]
         private static extern int lzfse_decode_buffer(byte[] decompressedBuffer, int decompressedSize, byte[] compressedBuffer, int compressedSize, int scratchBuffer = 0);
 
+        /// <summary>
+        /// Compresses <paramref name="buffer"/>
+        /// </summary>
+        /// <param name="buffer">The buffer to compress</param>
         public static byte[] Compress(byte[] buffer)
         {
             byte[] compressedBuffer = new byte[buffer.Length];
@@ -25,6 +32,12 @@ namespace LZFSESharp
             return compressedBuffer.Take(actualCompressedSize).ToArray();
         }
 
+        /// <summary>
+        /// Decompresses a LZFSE compressed buffer
+        /// </summary>
+        /// <param name="buffer">The buffer to decompress</param>
+        /// <param name="uncompressedSize">The uncompressed size of <paramref name="buffer"/></param>
+        /// <param name="decompressionSizeFactor">If <paramref name="uncompressedSize"/> is not provided, this is used to determine the size of the output buffer</param>
         public static byte[] Decompress(byte[] buffer, int uncompressedSize = 0, int decompressionSizeFactor = 8)
         {
             int decompressedSize = uncompressedSize == 0 ? buffer.Length * decompressionSizeFactor : uncompressedSize;
