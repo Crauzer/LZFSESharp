c62401419147658d0446535a7789b4ad2ff7e089
Crauzer <filip.quitko@gmail.com>
2017-12-30 02:39:16 +0100

    LZFSESharp

---
+++ LZFSESharp.Tests/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LZFSESharp;
+using System.IO;
+
+namespace LZFSESharp.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 3)
+            {
+                Console.WriteLine("Usage: -compress <input file path> <output file path>\n" +
+                                  "       -decompress <input file path> <output file path>");
+                return;
+            }
+
+            OperationType operationType = args[0] == "-compress" ? OperationType.Compress : OperationType.Decompress;
+            if(operationType == OperationType.Compress)
+            {
+                File.WriteAllBytes(args[2], LZFSE.Compress(File.ReadAllBytes(args[1])));
+            }
+            else
+            {
+                File.WriteAllBytes(args[2], LZFSE.Decompress(File.ReadAllBytes(args[1])));
+            }
+        }
+    }
+
+    public enum OperationType
+    {
+        Compress,
+        Decompress
+    }
+}
---
+++ LZFSESharp/LZFSE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace LZFSESharp
+{
+    public static class LZFSE
+    {
+        [DllImport("lzfse.dll")]
+        private static extern int lzfse_encode_buffer(byte[] compressedBuffer, int compressedSize, byte[] decompressedBuffer, int decompressedSize, int scratchBuffer = 0);
+
+        [DllImport("lzfse.dll")]
+        private static extern int lzfse_decode_buffer(byte[] decompressedBuffer, int decompressedSize, byte[] compressedBuffer, int compressedSize, int scratchBuffer = 0);
+
+        public static byte[] Compress(byte[] buffer)
+        {
+            byte[] compressedBuffer = new byte[buffer.Length];
+            int actualCompressedSize = lzfse_encode_buffer(compressedBuffer, buffer.Length, buffer, buffer.Length);
+
+            if (actualCompressedSize == 0)
+            {
+                throw new Exception("There was an error compressing the specified buffer.");
+            }
+
+            return compressedBuffer.Take(actualCompressedSize).ToArray();
+        }
+
+        public static byte[] Decompress(byte[] buffer)
+        {
+            int decompressedSize = buffer.Length * 4;
+            byte[] decompressedBuffer = new byte[decompressedSize];
+            int actualDecompressedSize = lzfse_decode_buffer(decompressedBuffer, decompressedSize, buffer, buffer.Length);
+
+            if (actualDecompressedSize == 0)
+            {
+                throw new Exception("There was an error decompressing the specified buffer.");
+            }
+
+            return decompressedBuffer.Take(actualDecompressedSize).ToArray();
+        }
+    }
+}
